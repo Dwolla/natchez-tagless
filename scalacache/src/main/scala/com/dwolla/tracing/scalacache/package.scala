5829df002c1fe9a93f697f3032e954cad2c052cf
Brian Holt <bholt@dwolla.com>
2022-11-08 19:11:58 -0600

    capture both inputs and outputs for scalacache.Cache

--- scalacache/src/main/scala/com/dwolla/tracing/scalacache/package.scala
+++ scalacache/src/main/scala/com/dwolla/tracing/scalacache/package.scala
@@ -7,7 +7,7 @@ import cats.tagless.aop._
 import com.dwolla.tracing._
 import io.circe.Codec
 import io.circe.generic.semiauto._
-import natchez.Trace
+import natchez._
 
 import scala.concurrent.duration.Duration
 
@@ -17,29 +17,30 @@ package object scalacache {
   implicit val flagsEncoder: Codec[Flags] = deriveCodec
 
   // unfortunately, due to the F[V] parameter in cachingF, this transformation cannot be automatically derived
-  def weaveCache[F[_], K: ToTraceValue, V: ToTraceValue](af: Cache[F, K, V]): Cache[Aspect.Weave.Domain[F, ToTraceValue, *], K, V] =
-    new Cache[Aspect.Weave.Domain[F, ToTraceValue, *], K, V] {
-      override def get(key: K)(implicit flags: Flags): Aspect.Weave.Domain[F, ToTraceValue, Option[V]] =
+  // (see the comment in CacheWeaveFunctionK below)
+  def weaveCache[F[_], K: ToTraceValue, V: ToTraceValue](af: Cache[F, K, V]): Cache[Aspect.Weave[F, ToTraceValue, ToTraceValue, *], K, V] =
+    new Cache[Aspect.Weave[F, ToTraceValue, ToTraceValue, *], K, V] {
+      override def get(key: K)(implicit flags: Flags): Aspect.Weave[F, ToTraceValue, ToTraceValue, Option[V]] =
         Aspect.Weave("Cache", List(
           List(Aspect.Advice("key", Eval.now(key))),
           List(Aspect.Advice("flags", Eval.now(flags))),
         ), Aspect.Advice("get", af.get(key)))
 
       override def put(key: K)
-                      (value: V, ttl: Option[Duration])(implicit flags: Flags): Aspect.Weave.Domain[F, ToTraceValue, Unit] =
+                      (value: V, ttl: Option[Duration])(implicit flags: Flags): Aspect.Weave[F, ToTraceValue, ToTraceValue, Unit] =
         Aspect.Weave("Cache", List(
           List(Aspect.Advice("key", Eval.now(key))),
           List(Aspect.Advice("value", Eval.now(value)), Aspect.Advice("ttl", Eval.now(ttl))),
           List(Aspect.Advice("flags", Eval.now(flags))),
         ), Aspect.Advice("put", af.put(key)(value, ttl)))
 
-      override def remove(key: K): Aspect.Weave.Domain[F, ToTraceValue, Unit] =
+      override def remove(key: K): Aspect.Weave[F, ToTraceValue, ToTraceValue, Unit] =
         Aspect.Weave("Cache", List(List(Aspect.Advice("key", Eval.now(key)))), Aspect.Advice("remove", af.remove(key)))
 
-      override def removeAll: Aspect.Weave.Domain[F, ToTraceValue, Unit] =
+      override def removeAll: Aspect.Weave[F, ToTraceValue, ToTraceValue, Unit] =
         Aspect.Weave("Cache", List.empty, Aspect.Advice("removeAll", af.removeAll))
 
-      override def caching(key: K)(ttl: Option[Duration])(f: => V)(implicit flags: Flags): Aspect.Weave.Domain[F, ToTraceValue, V] =
+      override def caching(key: K)(ttl: Option[Duration])(f: => V)(implicit flags: Flags): Aspect.Weave[F, ToTraceValue, ToTraceValue, V] =
         Aspect.Weave("Cache", List(
           List(Aspect.Advice("key", Eval.now(key))),
           List(Aspect.Advice("ttl", Eval.now(ttl))),
@@ -47,7 +48,7 @@ package object scalacache {
           List(Aspect.Advice("flags", Eval.now(flags))),
         ), Aspect.Advice("caching", af.caching(key)(ttl)(f)))
 
-      override def cachingF(key: K)(ttl: Option[Duration])(f: Aspect.Weave.Domain[F, ToTraceValue, V])(implicit flags: Flags): Aspect.Weave.Domain[F, ToTraceValue, V] =
+      override def cachingF(key: K)(ttl: Option[Duration])(f: Aspect.Weave[F, ToTraceValue, ToTraceValue, V])(implicit flags: Flags): Aspect.Weave[F, ToTraceValue, ToTraceValue, V] =
         Aspect.Weave("Cache", List(
           List(Aspect.Advice("key", Eval.now(key))),
           List(Aspect.Advice("ttl", Eval.now(ttl))),
@@ -55,7 +56,7 @@ package object scalacache {
           List(Aspect.Advice("flags", Eval.now(flags))),
         ), Aspect.Advice("cachingF", af.cachingF(key)(ttl)(f.codomain.target)))
 
-      override def close: Aspect.Weave.Domain[F, ToTraceValue, Unit] =
+      override def close: Aspect.Weave[F, ToTraceValue, ToTraceValue, Unit] =
         Aspect.Weave("Cache", List.empty, Aspect.Advice("close", af.close))
     }
 
@@ -64,17 +65,22 @@ package object scalacache {
 
 package scalacache {
   class CacheOps[F[_], K, V](val cache: Cache[F, K, V]) extends AnyVal {
-    def weave(implicit K: ToTraceValue[K], V: ToTraceValue[V]): Cache[Aspect.Weave.Domain[F, ToTraceValue, *], K, V] =
+    def weave(implicit K: ToTraceValue[K], V: ToTraceValue[V]): Cache[Aspect.Weave[F, ToTraceValue, ToTraceValue, *], K, V] =
       weaveCache(cache)
 
-    def weaveTracing(implicit F: Apply[F], T: Trace[F], K: ToTraceValue[K], V: ToTraceValue[V]): Cache[F, K, V] =
-      InvariantK[Cache[*[_], K, V]].imapK(cache.weave)(new TraceWeaveCapturingInputs)(new CacheWeaveFunctionK[F])
+    def weaveTracing(implicit F: FlatMap[F], T: Trace[F], K: ToTraceValue[K], V: ToTraceValue[V]): Cache[F, K, V] =
+      InvariantK[Cache[*[_], K, V]].imapK(cache.weave)(new TraceWeaveCapturingInputsAndOutputs)(new CacheWeaveFunctionK[F])
   }
 
-  private class CacheWeaveFunctionK[F[_]] extends (F ~> Aspect.Weave.Domain[F, ToTraceValue, *]) {
-    override def apply[A](fa: F[A]): Aspect.Weave.Domain[F, ToTraceValue, A] =
-    // This is kind of cheating; it takes advantage of the fact that F[_] only
-    // appears in a contravariant position in the cachingF method
+  private class CacheWeaveFunctionK[F[_]] extends (F ~> Aspect.Weave[F, ToTraceValue, ToTraceValue, *]) {
+    override def apply[A](fa: F[A]): Aspect.Weave[F, ToTraceValue, ToTraceValue, A] = {
+      implicit val faToTraceValue: ToTraceValue[A] = _ => "unevaluated F[A] effect"
+
+      // This seems like it's kind of cheating; it takes advantage of the fact that F[_]
+      // only appears in a contravariant position in the cachingF method. We don't capture
+      // its value, but we do capture that specific method name. AFAICT this doesn't
+      // matter as this value never appears in the trace attributes. 🤷
       Aspect.Weave("Cache", List.empty, Aspect.Advice("cachingF", fa))
+    }
   }
 }
