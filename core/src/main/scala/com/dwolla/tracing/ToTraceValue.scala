5f761f8ff0d2d2577e014189e85b829cf9f66884
Brian Holt <bholt@dwolla.com>
2023-01-21 18:18:44 -0600

    use TraceableValue.stringToTraceValue instead of TraceableValue[String] to avoid infinite recursion

--- core/src/main/scala/com/dwolla/tracing/ToTraceValue.scala
+++ core/src/main/scala/com/dwolla/tracing/ToTraceValue.scala
@@ -9,18 +9,19 @@ import natchez.{TraceValue, TraceableValue}
 object LowPriorityTraceableValueInstances extends LowPriorityTraceableValueInstances
 
 trait LowPriorityTraceableValueInstances extends LowestPriorityTraceableValueInstances {
-  implicit val unitTraceableValue: TraceableValue[Unit] = TraceableValue[String].contramap(_ => "()")
+  implicit val unitTraceableValue: TraceableValue[Unit] =
+    TraceableValue.stringToTraceValue.contramap(_ => "()")
 
-  implicit def optionalTraceValue[A: TraceableValue]: TraceableValue[Option[A]] = new TraceableValue[Option[A]] {
-    override def toTraceValue(a: Option[A]): TraceValue = a match {
-      case Some(a) => TraceableValue[A].toTraceValue(a)
-      case None => TraceValue.StringValue("None")
-    }
+  implicit def optionalTraceValue[A: TraceableValue]: TraceableValue[Option[A]] = {
+    case Some(a) => TraceableValue[A].toTraceValue(a)
+    case None => TraceValue.StringValue("None")
   }
 
-  implicit def traceValueViaJson[A: Encoder]: TraceableValue[A] = _.asJson.noSpaces
+  implicit def traceValueViaJson[A: Encoder]: TraceableValue[A] =
+    TraceableValue.stringToTraceValue.contramap(_.asJson.noSpaces)
 }
 
 trait LowestPriorityTraceableValueInstances {
-  implicit def traceValueViaShow[A: Show]: TraceableValue[A] = _.show
+  implicit def traceValueViaShow[A: Show]: TraceableValue[A] =
+    TraceableValue.stringToTraceValue.contramap(_.show)
 }
